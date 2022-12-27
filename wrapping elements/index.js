1c47b12340dbf10b8d1210f7a53f87549cb1874f
Adhil Salim <bizadhil@gmail.com>
2022-12-27 08:26:59 +0530

    created wrapping elements

---
+++ wrapping elements/index.js
@@ -0,0 +1,14 @@
+const navBar = (
+    <nav>
+        <ul>
+            <li>item one</li>
+            <li>item two</li>
+            <li>item three</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navBar
+    , document.getElementById('root')
+)
